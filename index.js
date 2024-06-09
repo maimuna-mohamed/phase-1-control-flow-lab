3e570951fe9e74d7fa58dccf2bd0c553d16fea00
maimuna-mohamed <maimunamohamedaden17@gmail.com>
2024-06-09 22:59:31 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  let result;
+  if (distance <= 400){
+    result ="This one is on me!";
+  }
+  else if (distance >2500){
+    result = "No can do.";
+  }
+  else{
+    result = "I will gladly take your thirty bucks.";
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city ==='NYC' ? "Ok, sounds good.": 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let response;
+  switch (tip){
+    case 'generous':
+      return 'Thank you so much.';
+    case 'not as generous':
+      return 'Thank you.';
+    default:
+          return'Bye.';
+  }
+  return response;
+
 }
